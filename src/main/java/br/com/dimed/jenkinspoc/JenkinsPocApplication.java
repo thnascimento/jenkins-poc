f05be3a874b8475c34fa03d31a654233aaf65826
nascimentot <nascimentot@grupodimed.com.br>
2019-09-12 16:32:43 -0300

    first commit

---
+++ src/main/java/br/com/dimed/jenkinspoc/JenkinsPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.dimed.jenkinspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPocApplication.class, args);
+	}
+
+}
